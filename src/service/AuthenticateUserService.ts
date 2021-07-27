abef026ddddbf7b6d2a2c7a8ca499ced3abcba5a
caioemanuel <caioemanuel@noar.tv>
2021-07-27 15:15:44 -0300

    create authiser controller

--- src/service/AuthenticateUserService.ts
+++ src/service/AuthenticateUserService.ts
@@ -1,23 +1,45 @@
 import { getCustomRepository } from "typeorm";
 import { UsersRepositories } from "../repositories/UsersRepositories";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface IAuthenticateUserRequest {
   email: string;
   password: string;
 }
 
-class AuthenticateUserRequest {
-  async execute({email, password} : IAuthenticateUserRequest) {
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateUserRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
-    
+
     //Verificar se usuário existe
     const user = await usersRepository.findOne({
-      email
-    })
+      email,
+    });
 
-    if(!user) {
+    if (!user) {
       throw new Error("Email/password incorrect");
     }
-    
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/password incorrect");
+    }
+
+    const token = sign(
+      {
+        email: user.email,
+      },
+      "67be988f498cd18fb2945aab0b51e031",
+      {
+        subject: user.id,
+        expiresIn: "",
+      }
+    );
+
+    return token;
   }
 }
+
+export { AuthenticateUserService };
