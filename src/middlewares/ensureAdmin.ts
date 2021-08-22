afd9b7f5a7b89e17bb887a13e5582d2af6531416
caioemanuel <caioemanuel@noar.tv>
2021-08-22 15:26:42 -0300

    update ensureAdmin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,19 +1,23 @@
 import { Request, Response, NextFunction } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(
+export async function ensureAdmin(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
+  const { user_id } = request;
 
-  const admin = true;
+  const usersRepositories = getCustomRepository(UsersRepositories);
 
-  if(admin) {
+  const { admin } = await usersRepositories.findOne(user_id);
+
+  if (admin) {
     return next();
   }
 
   return response.status(401).json({
-    error: "Unauthorized."
+    error: "Unauthorized.",
   });
-
 }
