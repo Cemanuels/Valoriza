52fba532707087581f7924a2c2b4c80b9be0e865
caioemanuel <caioemanuel@noar.tv>
2021-08-22 15:28:17 -0300

    update routes and tsconfig

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,8 @@ import { DeleteTagController } from "./controllers/DeleteTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 
 export const router = Router();
 
@@ -19,12 +21,17 @@ const createTagController = new CreateTagController();
 const deleteTagController = new DeleteTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 
-router.get("/users", getUsersController.handle);
+router.post("/login", authenticateUserController.handle);
 router.post("/users", createUserController.handle);
-router.delete("/users/:id", deleteUserController.handle);
+
+router.use(ensureAuthenticated);
+
+router.get("/users", ensureAdmin, getUsersController.handle);
+router.delete("/users/:id", ensureAdmin, deleteUserController.handle);
 router.get("/tags", getTagsController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
 router.delete("/tags/:id", deleteTagController.handle);
-router.post("/login", authenticateUserController.handle);
 router.post("/compliments", createComplimentController.handle);
+router.get("/compliments/receive", listUserReceiveComplimentsController.handle);
